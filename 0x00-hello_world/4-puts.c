bc9650ae1d0d4e862c2a7d43a67b3e6a489a4fc6
ijay2010 <umehijeoma2@gmail.com>
2023-02-17 12:08:18 -0800

    too hard

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual puzzle"
+ * main - prints "Programming is like building a multilingual puzzle."
  *
  * Return: 0
  */
@@ -10,7 +10,7 @@
 int main(void)
 
 {
-	puts("\"Programming is like building a multilingual puzzle";
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
